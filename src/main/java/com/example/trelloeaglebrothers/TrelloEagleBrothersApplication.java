a46cede84f666c5249eac6806afd27aefe2c8d96
BenKim95 <forseminaar@gmail.com>
2023-08-07 12:45:53 +0900

    init commit

---
+++ src/main/java/com/example/trelloeaglebrothers/TrelloEagleBrothersApplication.java
@@ -0,0 +1,13 @@
+package com.example.trelloeaglebrothers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloEagleBrothersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloEagleBrothersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trelloeaglebrothers/TrelloEagleBrothersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trelloeaglebrothers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloEagleBrothersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
