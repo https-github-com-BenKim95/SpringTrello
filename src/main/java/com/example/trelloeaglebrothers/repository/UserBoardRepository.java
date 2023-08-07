040091c4d11ce36123da4e03418e0d1d03357f40
Choo Hyunjoong <hjchoo19950327@gmail.com>
2023-08-08 00:12:51 +0900

    모든 Entity Repository interface 생성

---
+++ src/main/java/com/example/trelloeaglebrothers/repository/UserBoardRepository.java
@@ -0,0 +1,8 @@
+package com.example.trelloeaglebrothers.repository;
+
+import com.example.trelloeaglebrothers.entity.UserBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserBoardRepository extends JpaRepository<UserBoard, Long> {
+
+}
