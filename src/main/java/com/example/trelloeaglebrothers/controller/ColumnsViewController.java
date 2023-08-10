81f90697ab269f4993b7a5d193a5c9553ba2e0db
Choo Hyunjoong <hjchoo19950327@gmail.com>
2023-08-10 22:30:57 +0900

    columList 조회 기능 구현 및 프론트 기능 구현 중

---
+++ src/main/java/com/example/trelloeaglebrothers/controller/ColumnsViewController.java
@@ -0,0 +1,34 @@
+package com.example.trelloeaglebrothers.controller;
+
+import com.example.trelloeaglebrothers.dto.ColumnListResponseDto;
+import com.example.trelloeaglebrothers.security.UserDetailsImpl;
+import com.example.trelloeaglebrothers.service.ColumnListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ColumnsViewController {
+
+    private final ColumnListService columnListService;
+
+    @GetMapping("/board/{board_id}/column/column_list/{column_list_id}")
+    public ColumnListResponseDto getColumns(@PathVariable Long board_id,
+                          @PathVariable Long column_list_id,
+                          @AuthenticationPrincipal UserDetailsImpl userDetails
+                          ){
+
+        return columnListService.getColumn(userDetails.getUser(), board_id, column_list_id);
+
+
+
+        }
+
+
+
+}
