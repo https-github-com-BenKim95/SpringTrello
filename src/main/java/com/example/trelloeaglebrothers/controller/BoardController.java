d66e1d1d4570f6723a520f026e43211952c8470e
ohoh2302 <ssjj2302@gmail.com>
2023-08-11 21:29:31 +0900

    내 보드만 보이게 조회 기능 추가 수정 invite member 페이지 추가

--- src/main/java/com/example/trelloeaglebrothers/controller/BoardController.java
+++ src/main/java/com/example/trelloeaglebrothers/controller/BoardController.java
@@ -1,9 +1,10 @@
 package com.example.trelloeaglebrothers.controller;
 
-import com.example.trelloeaglebrothers.dto.AllResponseDto;
 import com.example.trelloeaglebrothers.dto.BoardRequestDto;
 import com.example.trelloeaglebrothers.dto.BoardResponseDto;
 import com.example.trelloeaglebrothers.dto.CollaboratorRequestDto;
+import com.example.trelloeaglebrothers.entity.Board;
+import com.example.trelloeaglebrothers.entity.User;
 import com.example.trelloeaglebrothers.security.UserDetailsImpl;
 import com.example.trelloeaglebrothers.service.BoardService;
 import com.example.trelloeaglebrothers.service.UserService;
@@ -30,36 +31,46 @@ public class BoardController {
         return boardService.getBoards();
     }
 
+
+
     @PostMapping("/board")
-    public BoardResponseDto createBoard(@RequestBody @Valid BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public BoardResponseDto createBoard(@RequestBody @Valid BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
         return boardService.createBoard(requestDto, userDetails.getUser());
     }
 
     @PutMapping("/board/{id}")
-    public BoardResponseDto updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public BoardResponseDto updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails)
+    {
         return boardService.updateBoard(id, requestDto, userDetails.getUser());
     }
 
     @DeleteMapping("/board/{id}")
-    public ResponseEntity<Message> deleteBoard(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<Message> deleteBoard(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails)
+    {
         return boardService.deleteBoard(id, userDetails.getUser());
     }
 
     @PostMapping("/board/collaborator/{boardId}")
     public ResponseEntity<Message> addCollaborator(@PathVariable Long boardId,
                                                    @RequestBody CollaboratorRequestDto collaboratorRequestDto,
-                                                   @AuthenticationPrincipal UserDetailsImpl userDetails) {
+                                                   @AuthenticationPrincipal UserDetailsImpl userDetails){
 
-        return boardService.addCollaborator(boardId, collaboratorRequestDto, userDetails.getUser());
+        return   boardService.addCollaborator(boardId, collaboratorRequestDto ,userDetails.getUser());
     }
 
-    //보드 단건 조회
-//    @GetMapping("/board/{id}")
-//    public AllResponseDto getBoard(@PathVariable Long id) {
-//        return boardService.getBoard(id);
-//    }
-//    }
+    @GetMapping("/board/{board_id}")
+    public ResponseEntity<BoardResponseDto> getBoard(@PathVariable Long board_id) {
+        BoardResponseDto boardResponseDto = boardService.getBoard(board_id);
+        return ResponseEntity.ok().body(boardResponseDto);
+    }
+
+
+    @GetMapping("/myboards")
+    public List<BoardResponseDto> getUserBoards(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        return boardService.getUserBoards(user);
+    }
+
+
+
 }
-
-
-
