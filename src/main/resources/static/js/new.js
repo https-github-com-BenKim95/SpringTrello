31bc1683f21d64656a6e5ba9fc3bae262463f0e6
BenKim95 <forseminaar@gmail.com>
2023-08-11 17:18:57 +0900

    푸름님 블랜치에서 templates 가져옴

--- src/main/resources/static/js/new.js
+++ src/main/resources/static/js/new.js
@@ -2,46 +2,46 @@ const draggables = document.querySelectorAll(".draggable");
 const containers = document.querySelectorAll(".container");
 
 draggables.forEach(draggable => {
-    draggable.addEventListener("dragstart", () => {
-        draggable.classList.add("dragging");
-    });
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
 
-    draggable.addEventListener("dragend", () => {
-        draggable.classList.remove("dragging");
-    });
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging");
+  });
 });
 
 containers.forEach(container => {
-    container.addEventListener("dragover", e => {
-        e.preventDefault();
-        const afterElement = getDragAfterElement(container, e.clientX);
-        const draggable = document.querySelector(".dragging");
-        if (afterElement === undefined) {
-            container.appendChild(draggable);
-        } else {
-            container.insertBefore(draggable, afterElement);
-        }
-    });
+  container.addEventListener("dragover", e => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientX);
+    const draggable = document.querySelector(".dragging");
+    if (afterElement === undefined) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
 });
 
 function getDragAfterElement(container, x) {
-    const draggableElements = [
-        ...container.querySelectorAll(".draggable:not(.dragging)"),
-    ];
+  const draggableElements = [
+    ...container.querySelectorAll(".draggable:not(.dragging)"),
+  ];
 
-    return draggableElements.reduce(
-        (closest, child) => {
-            const box = child.getBoundingClientRect();
-            const offset = x - box.left - box.width / 2;
-            // console.log(offset);
-            if (offset < 0 && offset > closest.offset) {
-                return { offset: offset, element: child };
-            } else {
-                return closest;
-            }
-        },
-        { offset: Number.NEGATIVE_INFINITY },
-    ).element;
+  return draggableElements.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = x - box.left - box.width / 2;
+        // console.log(offset);
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      { offset: Number.NEGATIVE_INFINITY },
+  ).element;
 }
 
 idx.init();
@@ -93,157 +93,157 @@ idx.init();
 // }
 
 function Create() {
-    let title = $('#title').val();
-    let color = $('#color').val();
-    let description = $('#description').val();
+  let title = $('#title').val();
+  let color = $('#color').val();
+  let description = $('#description').val();
 
-    console.log(title, color, description);
+  console.log(title, color, description);
 
-    $.ajax({
-        type: "POST",
-        url: "/api/board",
-        contentType: "application/json",
-        data: JSON.stringify({title: title, color: color, description: description}),
-        success: function(response, status, xhr) {
-            // 요청이 성공한 경우 처리할 로직을 작성합니다.
-            console.log("POST 요청이 성공했습니다.");
+  $.ajax({
+    type: "POST",
+    url: "/api/board",
+    contentType: "application/json",
+    data: JSON.stringify({title: title, color: color, description: description}),
+    success: function(response, status, xhr) {
+      // 요청이 성공한 경우 처리할 로직을 작성합니다.
+      console.log("POST 요청이 성공했습니다.");
 
-            // 서버 응답이 성공적으로 왔을 때 처리
-            if (xhr.status === 200) {
-                Swal.fire({
-                    icon: 'success',
-                    title: '보드 생성 완료!'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = "http://localhost:8080";
-                    }
-                });
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: '보드 생성 실패'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = "http://localhost:8080";
-                    }
-                });
-            }
-        },
-        error: function(xhr, status, error) {
-            // 요청이 실패한 경우 처리할 로직을 작성합니다.
-            console.log("POST 요청이 실패했습니다.");
-            console.log(xhr.responseText);
-            Swal.fire({
-                icon: 'error',
-                title: '보드 작성 실패'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = "http://localhost:8080";
-                }
-            });
+      // 서버 응답이 성공적으로 왔을 때 처리
+      if (xhr.status === 200) {
+        Swal.fire({
+          icon: 'success',
+          title: '보드 생성 완료!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = "http://localhost:8080";
+          }
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: '보드 생성 실패'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = "http://localhost:8080";
+          }
+        });
+      }
+    },
+    error: function(xhr, status, error) {
+      // 요청이 실패한 경우 처리할 로직을 작성합니다.
+      console.log("POST 요청이 실패했습니다.");
+      console.log(xhr.responseText);
+      Swal.fire({
+        icon: 'error',
+        title: '보드 작성 실패'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "http://localhost:8080";
         }
-    });
+      });
+    }
+  });
 }
 // 게시글 삭제
 function Delete() {
-    let url = window.location.href;
-    let id = url.replace("http://localhost:8080/api/board/", "");
-    console.log(id);
+  let url = window.location.href;
+  let id = url.replace("http://localhost:8080/api/board/", "");
+  console.log(id);
 
-    $.ajax({
-        type: "DELETE",
-        url: `/api/board/${id}`,
-        contentType: "application/json",
-        success: function(response, status, xhr) {
-            if (xhr.status === 200) {
-                // 요청이 성공한 경우 처리할 로직을 작성합니다.
-                console.log("DELETE 요청이 성공했습니다.");
-                Swal.fire({
-                    icon: 'success',
-                    title: '보드 삭제 완료!'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = "http://localhost:8080";
-                    }
-                });
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: '작성자만 삭제 가능합니다'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = `http://localhost:8080/api/post/${id}`;
-                    }
-                });
-            }
-        },
-        error: function(xhr, status, error) {
-            // 요청이 실패한 경우 처리할 로직을 작성합니다.
-            console.log("DELETE 요청이 실패했습니다.");
-            console.log(xhr.responseText);
-            Swal.fire({
-                icon: 'error',
-                title: '작성자만 삭제 가능합니다'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = `http://localhost:8080/api/post/${id}`;
-                }
-            });
+  $.ajax({
+    type: "DELETE",
+    url: `/api/board/${id}`,
+    contentType: "application/json",
+    success: function(response, status, xhr) {
+      if (xhr.status === 200) {
+        // 요청이 성공한 경우 처리할 로직을 작성합니다.
+        console.log("DELETE 요청이 성공했습니다.");
+        Swal.fire({
+          icon: 'success',
+          title: '보드 삭제 완료!'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = "http://localhost:8080";
+          }
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: '작성자만 삭제 가능합니다'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = `http://localhost:8080/api/post/${id}`;
+          }
+        });
+      }
+    },
+    error: function(xhr, status, error) {
+      // 요청이 실패한 경우 처리할 로직을 작성합니다.
+      console.log("DELETE 요청이 실패했습니다.");
+      console.log(xhr.responseText);
+      Swal.fire({
+        icon: 'error',
+        title: '작성자만 삭제 가능합니다'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = `http://localhost:8080/api/post/${id}`;
         }
-    });
+      });
+    }
+  });
 }
 
 function Edite() {
-    let params = new URLSearchParams(location.search);
-    let id = params.get('id');
+  let params = new URLSearchParams(location.search);
+  let id = params.get('id');
 
-    let title = $('#title').val();
-    let color = $('#color').val();
-    let description = $('#description').val();
+  let title = $('#title').val();
+  let color = $('#color').val();
+  let description = $('#description').val();
 
 
-    $.ajax({
-        type: "PUT",
-        url: `/api/board/${id}`,
-        contentType: "application/json",
-        data: JSON.stringify({title: title, color: color, description: description}),
-        success: function(response, status, xhr) {
-            // 요청이 성공한 경우 처리할 로직을 작성합니다.
-            console.log("PUT 요청이 성공했습니다.");
+  $.ajax({
+    type: "PUT",
+    url: `/api/board/${id}`,
+    contentType: "application/json",
+    data: JSON.stringify({title: title, color: color, description: description}),
+    success: function(response, status, xhr) {
+      // 요청이 성공한 경우 처리할 로직을 작성합니다.
+      console.log("PUT 요청이 성공했습니다.");
 
-            // 서버 응답이 성공적으로 왔을 때 처리
-            if (xhr.status === 200) {
-                Swal.fire({
-                    icon: 'success',                         // Alert 타입
-                    title: '보드 수정 완료!'        // Alert 제목
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = `http://localhost:8080/api/board/${id}`;
-                    }
-                });
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: '보드 수정 실패'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        window.location.href = `http://localhost:8080/api/board/${id}`;
-                    }
-                });
-            }
-        },
-        error: function(xhr, status, error) {
-            // 요청이 실패한 경우 처리할 로직을 작성합니다.
-            console.log("POST 요청이 실패했습니다.");
-            console.log(xhr.responseText);
-            Swal.fire({
-                icon: 'error',
-                title: '작성자만 수정 가능합니다'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.location.href = `http://localhost:8080/api/board/${id}`;
-                }
-            });
+      // 서버 응답이 성공적으로 왔을 때 처리
+      if (xhr.status === 200) {
+        Swal.fire({
+          icon: 'success',                         // Alert 타입
+          title: '보드 수정 완료!'        // Alert 제목
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = `http://localhost:8080/api/board/${id}`;
+          }
+        });
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: '보드 수정 실패'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = `http://localhost:8080/api/board/${id}`;
+          }
+        });
+      }
+    },
+    error: function(xhr, status, error) {
+      // 요청이 실패한 경우 처리할 로직을 작성합니다.
+      console.log("POST 요청이 실패했습니다.");
+      console.log(xhr.responseText);
+      Swal.fire({
+        icon: 'error',
+        title: '작성자만 수정 가능합니다'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = `http://localhost:8080/api/board/${id}`;
         }
-    });
+      });
+    }
+  });
 }
