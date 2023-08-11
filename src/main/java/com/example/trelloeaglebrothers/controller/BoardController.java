e072c055c82b6b08176a9d04b84c951dc17cfaf4
BenKim95 <forseminaar@gmail.com>
2023-08-11 15:30:42 +0900

    Cardcontroller 누락된거 추가

--- src/main/java/com/example/trelloeaglebrothers/controller/BoardController.java
+++ src/main/java/com/example/trelloeaglebrothers/controller/BoardController.java
@@ -30,6 +30,12 @@ public class BoardController {
         return boardService.getBoards();
     }
 
+    //보드 단건 조회
+    @GetMapping("/board/{id}")
+    public AllResponseDto getBoard(@PathVariable Long id) {
+        return boardService.getBoard(id);
+    }
+
     @PostMapping("/board")
     public BoardResponseDto createBoard(@RequestBody @Valid BoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return boardService.createBoard(requestDto, userDetails.getUser());
