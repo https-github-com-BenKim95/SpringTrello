3ba4f6b2ac8821076bb6a156300d78ca3e9892c8
ohoh2302 <ssjj2302@gmail.com>
2023-08-13 17:38:44 +0900

    프론트 - 멤버 초대 부분 구현 성공

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -45,10 +45,10 @@ function getBoards() {
                               </div>
                             </a>
                             <div class="d-grid gap-2 d-md-flex justify-content-md-center my-3">
-                              <button th:if="${boardId} != null" class="btn btn-light btn-sm" id="member" type="button" onclick="inviteMember()">Member</button>
+                              <button class="btn btn-light btn-sm member-button" data-boardid="${boardId}" type="button" onclick="inviteMember(${boardId})">Member</button>
                               <button th:if="${boardId} != null" class="btn btn-light btn-sm" id="modify-btn" type="button" onclick="enableEdit()">Edit</button>
                               <button th:if="${boardId} != null" class="btn btn-light btn-sm none" id="confirm-btn" type="button" onclick="saveChanges()">Confirm</button>
-                               <button class="btn btn-outline-light btn-sm delete-board-btn" data-boardid="${boardId}" type="button">Delete</button>
+                              <button class="btn btn-outline-light btn-sm delete-board-btn" data-boardid="${boardId}" type="button">Delete</button>
                             </div>
                           </div>
                     `;
@@ -109,5 +109,56 @@ function deleteBoard(boardId) {
             console.error("보드 삭제 중 에러 발생:", error);
         });
 }
+
+
+
+
+
+
+function inviteMember(boardId) {
+    const collaboratorId = prompt("초대할 사용자의 ID를 입력하세요:");
+    if (collaboratorId) {
+        const collaboratorRequestDto = {
+            id: collaboratorId
+
+        };
+
+        fetch(`/api/board/collaborator/${boardId}`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(collaboratorRequestDto)
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    return response.json().then(data => {
+                        throw new Error(data.message);
+                    });
+                }
+            })
+            .then(data => {
+                console.log("멤버 초대 성공:", data.message);
+                // 멤버를 초대한 후 보드 목록을 다시 불러옵니다.
+                getBoards();
+            })
+            .catch(error => {
+                console.error("멤버 초대 중 에러 발생:", error);
+            });
+    }
+}
+
+
+function getBoardIdFromUrl() {
+    const url = window.location.href;
+    const boardIdPattern = /\/board\/(\d+)/;
+    const matches = url.match(boardIdPattern);
+    if (matches && matches.length >= 2) {
+        return matches[1];
+    }
+    return null;
+}
+
+
 
 
