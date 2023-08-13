bec4348fc4b3a5d5068419eac3c2c90e8d2ba559
jangwon kim <seed0335@gmail.com>
2023-08-13 23:30:37 +0900

    카드 생성 기능 추가

--- src/main/java/com/example/trelloeaglebrothers/dto/CardRequestDto.java
+++ src/main/java/com/example/trelloeaglebrothers/dto/CardRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.trelloeaglebrothers.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Setter
 @Getter
 public class CardRequestDto {
     private String title;
